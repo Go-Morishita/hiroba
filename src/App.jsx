f1c79e0d85114bd8b593cfe5147450342360d0dd
GO0128 <go20040128@gmail.com>
2023-09-22 14:39:13 +0900

    Add: topページの土台を作成しました。

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './style.scss'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='red'>こんにちは</h1>
+      <Home />
     </>
   )
 }
